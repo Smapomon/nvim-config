e57a454490e395c769199a60f557e4767c1a136a
Smapo <sahlaster95@gmail.com>
2022-09-28 16:07:44 +0300

    add more ripgrep binds

--- lua/smapo/keybinds.lua
+++ lua/smapo/keybinds.lua
@@ -16,11 +16,9 @@ end
 
 
 -- *************************************************************************** --
---------------------------
--- 			--
+----------------
 --	KEY MAPS	--
--- 			--
---------------------------
+----------------
 
 map('n', '*', '*N') -- Keep cursor position when starting search
 
@@ -237,6 +235,7 @@ map('n', '<Leader>hw', ':%!xxd -r<CR> :set binary<CR> :set filetype=<CR>')
 ---------------
 cmd[[vnoremap // y/\V<C-R>=escape(@",'/\')<CR><CR>]]
 map('n', '<Leader>f', ':Rg<CR>')
+map('n', '<Leader>r', ':Rg ')
 
 -- Search files for visually selected text
 cmd[[xnoremap <leader>f "zy :let cmd = 'Rg ' . @z <bar> call histadd("cmd", cmd) <bar> execute cmd <cr>]]
