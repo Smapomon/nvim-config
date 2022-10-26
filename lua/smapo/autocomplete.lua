04d9e1e322ea51cdd4be4437a6756e0d1f9ec21b
Sampo Sahla <sahlaster95@gmail.com>
2022-10-26 11:42:53 +0300

    release tab from autocomplete

--- lua/smapo/autocomplete.lua
+++ lua/smapo/autocomplete.lua
@@ -36,7 +36,7 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
     -- luasnip supertab combo
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-n>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -48,7 +48,7 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["C-p"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
