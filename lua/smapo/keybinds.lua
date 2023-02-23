ef43e9811ffa09b28c98c5a901dc36207dbeac52
secSmapo <sampo.sahla@atmotics.com>
2023-02-23 18:08:52 +0200

    change diagnostic window keybind

--- lua/smapo/keybinds.lua
+++ lua/smapo/keybinds.lua
@@ -33,8 +33,7 @@ map('n', '<C-p>', function()
   editor.cmd[[ :call fzf#vim#files('.', {'options': '--prompt "" --layout=reverse --preview="bat --style=numbers --color=always {}"'}) ]]
 end)
 
-map('n', '<C-ö>', '<cmd>TroubleToggle document_diagnostics<CR>')
-
+map('n', '<C-ö>', '<cmd>Trouble document_diagnostics<CR>')
 
 -------------------
 -- unbind arrows --
