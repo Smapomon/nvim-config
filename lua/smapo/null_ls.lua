82b7f5758b688a14929d60f51844501ba6373fcc
Smapomon <sahlaster95@gmail.com>
2023-12-03 01:52:23 +0200

    remove comments

--- lua/smapo/null_ls.lua
+++ lua/smapo/null_ls.lua
@@ -42,29 +42,3 @@ null_ls.setup({
   }
 })
 
--- CUSTOM CODE_ACTIONS
---local frozen_string_actions = {
-  --method = null_ls.methods.CODE_ACTION,
-  --filetypes = { "ruby" },
-  --generator = {
-    --fn = function(context)
-      --local frozen_string_literal_comment = "# frozen_string_literal: true"
-      --local first_line                    = context.content[1]
-
-      --if first_line == frozen_string_literal_comment then
-        --return {
-          --{
-            --title = "🥶Add frozen string literal comment",
-            --action = function()
-              --local lines = {frozen_string_literal_comment, "", first_line}
-              --editor.api.nvim_buf_set_lines(context.bufnr, 0, 1, false, lines)
-            --end
-          --}
-        --}
-      --end
-    --end
-  --}
---}
-
---null_ls.register(frozen_string_actions)
-
