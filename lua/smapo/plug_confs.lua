bbc044a62557da539a8530647a7a90af598b01c6
Sampo Sahla <sampo.sahla@atmotics.com>
2025-07-28 13:44:22 +0300

    show hidden dirs by default

--- lua/smapo/plug_confs.lua
+++ lua/smapo/plug_confs.lua
@@ -416,6 +416,10 @@ require('oil').setup({
   skip_confirm_for_simple_edits = true,
   constrain_cursor = "name",
 
+  view_options = {
+    show_hidden = true,
+  },
+
   columns = {
     "mtime",
     "size",
