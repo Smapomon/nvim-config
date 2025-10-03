ccf9727bba1405817681b6882775f4dfd84a559e
Smapomon <sahlaster95@gmail.com>
2025-10-03 13:00:26 +0300

    update completions

--- lua/plugins/complete.lua
+++ lua/plugins/complete.lua
@@ -1,29 +1,45 @@
 ---@diagnostic disable: undefined-global
 
+local kind_icons = {
+	Text = " ",
+	Method = "󰆧 ",
+	Function = "󰊕 ",
+	Constructor = " ",
+	Field = "󰇽 ",
+	Variable = "󰂡 ",
+	Class = "󰠱 ",
+	Interface = " ",
+	Module = " ",
+	Property = "󰜢 ",
+	Unit = " ",
+	Value = "󰎠 ",
+	Enum = "",
+	Keyword = "󰌋 ",
+	Snippet = " ",
+	Color = "󰏘 ",
+	File = "󰈙 ",
+	Reference = " ",
+	Folder = "󰉋 ",
+	EnumMember = " ",
+	Constant = "󰏿 ",
+	Struct = " ",
+	Event = " ",
+	Operator = "󰆕 ",
+	TypeParameter = "󰅲 ",
+	supermaven = " ",
+	Supermaven = " ",
+}
+
 return {
-	{
-		"github/copilot.vim",
-		cmd = "Copilot",
-		event = "BufWinEnter",
-		init = function()
-			vim.g.copilot_no_maps = true
-		end,
-		config = function()
-			-- Block the normal Copilot suggestions
-			vim.api.nvim_create_augroup("github_copilot", { clear = true })
-			vim.api.nvim_create_autocmd({ "FileType", "BufUnload" }, {
-				group = "github_copilot",
-				callback = function(args)
-					vim.fn["copilot#On" .. args.event]()
-				end,
-			})
-			vim.fn["copilot#OnFileType"]()
-		end,
-	},
+  {
+    'saghen/blink.compat',
+    version = '2.*', -- use v2.* for blink.cmp v1.*
+    lazy = true, -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    opts = {}, -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+  },
 	{
 		"saghen/blink.cmp",
 		dependencies = {
-			"fang2hou/blink-copilot",
 			{
 				"L3MON4D3/LuaSnip",
 				version = "v2.*",
@@ -42,19 +58,49 @@ return {
 					delete_check_events = "TextChanged",
 				},
 			},
-		},
+      "saghen/blink.compat",
+      {
+        "supermaven-inc/supermaven-nvim",
+        opts = {
+          disable_inline_completion = true, -- disables inline completion for use with cmp
+          disable_keymaps = true            -- disables built in keymaps for more manual control
+        }
+      },
+      {
+        "huijiro/blink-cmp-supermaven"
+      },
+    },
 
 		version = "1.*",
 
 		opts = {
-			-- C-space: Open menu or open docs if already open
+			-- C-n: Open menu if not already open
 			-- C-n/C-p or Up/Down: Select next/previous item
 			-- C-e: Hide menu
-			-- C-k: Toggle signature help (if signature.enabled = true)
-			keymap = { preset = "enter" },
+			-- C-k: Toggle documentation
+			keymap = {
+        preset = "enter",
 
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings'},
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<C-k>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        --['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+      },
 			appearance = {
 				nerd_font_variant = "mono",
+        kind_icons = kind_icons,
 			},
 
 			cmdline = {
@@ -85,6 +131,7 @@ return {
 							{ "label", "label_description", gap = 2 },
 							{ "kind_icon", "kind" },
 						},
+            treesitter = { "lsp" },
 					},
 				},
 			},
@@ -97,15 +144,19 @@ return {
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			snippets = { preset = "luasnip" },
 			sources = {
-				default = { "lsp", "path", "copilot", "snippets", "buffer" },
-				providers = {
-					copilot = {
-						name = "copilot",
-						module = "blink-copilot",
-						score_offset = 100,
-						async = true,
-					},
-				},
+        completion = {
+          enabled_providers = {}
+        },
+				default = { "lsp", "path", "snippets", "supermaven", "buffer" },
+        providers = {
+          supermaven = {
+            name = "supermaven",
+            module = "blink-cmp-supermaven",
+            score_offset = 3,
+            enabled = true,
+            async = true,
+          },
+        },
 			},
 
 			-- See the fuzzy documentation for more information
