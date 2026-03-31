50f5fc576a2c895d7509e3c8a8b80c7041ff71d3
Smapomon <sahlaster95@gmail.com>
2026-03-31 17:21:39 +0300

    update tree-sitter and some plugins

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -43,29 +43,14 @@ return {
 		},
 	},
 	{
+    -- If coming from old system remember to run:
+    -- `sudo pacman -Rns tree-sitter-grammars`
+    -- and in nvim: `TSInstall bash c diff html lua luadoc markdown markdown_inline query vim vimdoc`
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
+		branch = "main",
 		lazy = false,
 		build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash", "c", "diff", "html", "lua",
-          "luadoc", "markdown", "markdown_inline",
-          "query", "vim", "vimdoc",
-        },
-        -- Autoinstall languages that are not installed
-        auto_install = true,
-        highlight = {
-          enable = true,
-          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-          --  If you are experiencing weird indenting issues, add the language to
-          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-          additional_vim_regex_highlighting = { "ruby" },
-        },
-        indent = { enable = true, disable = { "ruby" } },
-      })
-    end,
+    opts = {},
 	},
 	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
