5c9b3eca97bfdf47b7da4ae6e9211493e3a82deb
Smapomon <sahlaster95@gmail.com>
2026-04-10 23:00:05 +0300

    makes solargraph properly work

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -182,7 +182,9 @@ return {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				solargraph = {},
+				solargraph = {
+					cmd = { "bundle", "exec", "solargraph", "stdio" },
+				},
 				lua_ls = {
 					settings = {
 						Lua = {
